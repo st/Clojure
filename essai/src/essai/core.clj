2c6e32c312e0120a0727004629648cf587ce7fb2
Stéphane Tavera <stephane@inagua.ch>
2011-10-09 18:47:56 +0200

    tic tac toe WIP.

--- essai/src/essai/core.clj
+++ essai/src/essai/core.clj
@@ -6,4 +6,37 @@
 (defn keep-capitals [s]
     (apply str (filter #(Character/isUpperCase %) (seq s))))
     
+(defn word-sorting [s]
+    (sort-by #(.toLowerCase %) (.split (.replaceAll s "[^a-zA-Z ]""") " "))) 
+    
+(defn- line-for? [l c]
+    (every? #(= c %) l))
+    
+(defn line-for-x? [l] 
+    (line-for? l :x))
+    
+(defn line-for-o? [l] 
+    (line-for? l :o))
 
+(defn at [v x y]
+    (-> v (nth y) (nth x)))
+
+(defn cols [v]
+    (for [x (range (count v))] (map #(nth % x) v)))
+
+(defn diag-up [v]
+    (let [len (count v)]
+        (for [x (range len)]
+            (at v x (- (dec len) x)))))
+            
+(defn diag-down [v]
+    (let [len (count v)]
+        (for [x (range len)]
+            (at v x x))))
+
+(defn find-winner [v]
+    (let [all (conj (map conj v (cols v) (diag-down v) (diag-up v))]
+        (cond
+            (some line-for-x? all) :x
+            (some line-for-o? all) :o
+            :else nil)))
--- essai/test/essai/core/test.clj
+++ essai/test/essai/core/test.clj
@@ -8,7 +8,71 @@
 (deftest dummy
     (is (= 1 1)))
         
-(deftest test-one
+(deftest test-get-caps
     (is (= ""       (keep-capitals "abc"))) 
     (is (= "AC"     (keep-capitals "AbC")))
     (is (= "ABC"    (keep-capitals "ABC")))) 
+    
+    
+(deftest test-word-sorting    
+    (is (= (word-sorting  "Have a nice day.")                   ["a" "day" "Have" "nice"]))
+    (is (= (word-sorting  "Clojure is a fun language!")         ["a" "Clojure" "fun" "is" "language"]))
+    (is (= (word-sorting  "Fools fall for foolish follies.")    ["fall" "follies" "foolish" "Fools" "for"])))
+    
+(deftest test-detect-x
+    (is (not (line-for-x?   [:e :e :e])))
+    (is (not (line-for-x?   [:x :x :e])))
+    (is (line-for-x?        [:x :x :x])))
+
+(deftest test-detect-o
+    (is (not (line-for-o?   [:e :e :e])))
+    (is (not (line-for-o?   [:o :o :e])))
+    (is (line-for-o?        [:o :o :o])))
+ 
+(deftest test-extract-cols
+    (is (= [[:x :x :x] [:e :e :e] [:e :x :e]]
+        (cols [ [:x :e :e]
+                [:x :e :x]
+                [:x :e :e]]))))
+                
+ (deftest test-diag-up
+     (is (= [:x :e :x]
+         (diag-up [ [:o :e :x]
+                    [:x :e :e]
+                    [:x :e :o]]))))
+                    
+(deftest test-diag-down
+    (is (= [:o :e :o]
+        (diag-down [ [:o :e :x]
+                     [:x :e :e]
+                     [:x :e :o]]))))
+
+
+(deftest test-find-winner
+    (is (= nil (find-winner [[:e :e :e]
+                            [:e :e :e]
+                            [:e :e :e]])))
+    
+    (is (= :x (find-winner [[:x :e :o]
+                            [:x :e :e]
+                            [:x :e :o]])))
+    
+    (is (= :o (find-winner [[:e :x :e]
+                            [:o :o :o]
+                            [:x :e :x]])))
+     
+    (is (= nil (find-winner [[:x :e :o]
+                            [:x :x :e]
+                            [:o :x :o]])))
+     
+    (is (= :x (find-winner [[:x :e :e]
+                            [:o :x :e]
+                            [:o :e :x]])))
+     
+    (is (= :o (find-winner [[:x :e :o]
+                            [:x :o :e]
+                            [:o :e :x]])))
+     
+    (is (= nil (find-winner [[:x :o :x]
+                            [:x :o :x]
+                            [:o :x :o]]))))
